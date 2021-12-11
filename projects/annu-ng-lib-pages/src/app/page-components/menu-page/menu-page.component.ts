e8216c8e57dd570d11afdb064a486f471f807e87
Sunil Kumar <sunil.divyam@gmail.com>
2021-12-11 23:44:05 +0530

    Menu Component - animation

--- projects/annu-ng-lib-pages/src/app/page-components/menu-page/menu-page.component.ts
+++ projects/annu-ng-lib-pages/src/app/page-components/menu-page/menu-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, Routes } from '@angular/router';
+import { ROUTE_TYPES } from '../../constants';
 
 @Component({
   selector: 'anu-menu-page',
@@ -10,7 +11,7 @@ export class MenuPageComponent implements OnInit {
   routes: Routes;
 
   constructor(private router: Router) {     
-    this.routes = this.router.config.filter(r => r.data?.type === 'main-navigation');
+    this.routes = this.router.config.filter(r => r.data?.type === ROUTE_TYPES.main);
   }
 
   ngOnInit(): void {
