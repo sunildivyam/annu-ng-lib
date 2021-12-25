cf918520281635f13970d56c6b90387545f5761f
Sunil Kumar <sunil.divyam@gmail.com>
2021-12-25 12:46:46 +0530

    Issie - fixed - Prod & prerender- build - all css vars are being converted from camel case to dash case.
    https://github.com/sunildivyam/annu-ng-lib/issues/8

--- projects/annu-ng-lib/src/lib/components/theme/theme.service.ts
+++ projects/annu-ng-lib/src/lib/components/theme/theme.service.ts
@@ -11,6 +11,7 @@ const DEFAULT_THEME = 'default';
 const PALETTE_COLOR_COUNT = 8;
 const PALETTE_LIGHTNESS_START = 10;
 const PALETTE_SHADES = ['DeepDark', 'Darkest', 'Darker', 'Dark', 'Normal', 'Light', 'Lighter', 'Lightest', 'DeepLight'];
+const CSS_VARIABLES_PREFIX = '--anu-';
 
 @Injectable({
   providedIn: 'root'
@@ -27,6 +28,12 @@ export class ThemeService {
     this.selectedThemeName.subscribe(themeName => this.loadTheme(themeName));
   }
 
+  // Replaces all upperCase letters to lowercase precided with '-' dash
+  private camelCaseToDashCase(str: string): string {
+    if (!str) return '';
+    return str.split('').map(c => c === c.toUpperCase() ? `-${c.toLowerCase()}` : c).join('');
+  }
+
   /*
   * get css var name and value.
   */
@@ -36,7 +43,8 @@ export class ThemeService {
     }
 
     if (value) {
-      name = '--anu-' + name;
+
+      name = CSS_VARIABLES_PREFIX + this.camelCaseToDashCase(name);
       return { name, value } as CssVar;
     }
 
@@ -49,7 +57,7 @@ export class ThemeService {
   private writeCssVarToDom(name: string, value: string = ''): void {
     const cssVar = this.getCssVar(name, value);
     if (cssVar) {
-      this.renderer.setStyle(this.document.documentElement, cssVar.name, cssVar.value, RendererStyleFlags2.DashCase);
+      this.renderer.setStyle(this.document.documentElement, cssVar.name, cssVar.value);
     }
   }
 
@@ -135,7 +143,7 @@ export class ThemeService {
       if (!themes[themeName]) {
         themeName = DEFAULT_THEME;
       }
-    } else if (!themes[themeName]){
+    } else if (!themes[themeName]) {
       themeName = DEFAULT_THEME;
     }
 
