093eaa5da14643972cb1933efcbbc57a946f676e
Sunil Kumar <sunil.divyam@gmail.com>
2022-02-06 15:15:10 +0530

    ServiceMethodPage component

---
+++ projects/annu-ng-lib-pages/src/app/page-components/service-method-page/service-method-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'anu-service-method-page',
+  templateUrl: './service-method-page.component.html',
+  styleUrls: ['./service-method-page.component.scss']
+})
+export class ServiceMethodPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
