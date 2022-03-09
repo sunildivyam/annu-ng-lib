ac330ef3002a1b0ec016de71e52b45c229be966e
Sunil Kumar <sunil.divyam@gmail.com>
2022-03-09 14:55:40 +0530

    AsideNav Component - common ui- enhancements

--- projects/annu-ng-lib/src/lib/components/common-ui/aside-nav/aside-nav.module.ts
+++ projects/annu-ng-lib/src/lib/components/common-ui/aside-nav/aside-nav.module.ts
@@ -4,16 +4,19 @@ import { AsideNavComponent } from './aside-nav.component';
 import { RouterModule } from '@angular/router';
 import { HamburgerModule } from '../hamburger';
 import { AsideNavItemComponent } from './aside-nav-item/aside-nav-item.component';
+import { AsideNavHeaderComponent } from './aside-nav-header/aside-nav-header.component';
+import { DrawerModule } from '../drawer';
 
 
 
 @NgModule({
-  declarations: [AsideNavComponent, AsideNavItemComponent],
+  declarations: [AsideNavComponent, AsideNavItemComponent, AsideNavHeaderComponent],
   imports: [
     CommonModule,
     RouterModule,
     HamburgerModule,
+    DrawerModule,
   ],
-  exports: [AsideNavComponent, AsideNavItemComponent]
+  exports: [AsideNavComponent, AsideNavItemComponent, AsideNavHeaderComponent]
 })
 export class AsideNavModule { }
--- projects/annu-ng-lib/src/lib/components/common-ui/aside-nav/index.ts
+++ projects/annu-ng-lib/src/lib/components/common-ui/aside-nav/index.ts
@@ -2,3 +2,4 @@ export * from './aside-nav.module';
 export * from './aside-nav.interface';
 export * from './aside-nav.component';
 export * from './aside-nav-item/aside-nav-item.component';
+export * from './aside-nav-header/aside-nav-header.component';
