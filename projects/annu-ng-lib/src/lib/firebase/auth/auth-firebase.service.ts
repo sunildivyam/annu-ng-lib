accc343fdfec6d16c3297938b26753f4dbd32e9c
Sunil Kumar <sunil.divyam@gmail.com>
2022-12-06 18:50:45 +0530

    storing auth token to localstorage

--- projects/annu-ng-lib/src/lib/firebase/auth/auth-firebase.service.ts
+++ projects/annu-ng-lib/src/lib/firebase/auth/auth-firebase.service.ts
@@ -1,4 +1,7 @@
-import { Injectable } from '@angular/core';
+import { FirebaseApp, getApps, initializeApp } from 'firebase/app';
+import { LibConfig } from '../../app-config/app-config.interface';
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { getAuth, User, Auth, onAuthStateChanged } from 'firebase/auth';
 import { Observable, Subject } from 'rxjs';
 import { CommonFirebaseService } from '../common-firebase';
@@ -10,11 +13,14 @@ export class AuthFirebaseService {
   auth: Auth;
   authState: Subject<User> = new Subject<User>();
 
-  constructor(private commonFireSvc: CommonFirebaseService) {
+  constructor(private commonFireSvc: CommonFirebaseService, @Inject(PLATFORM_ID) private platformId, private libConfig: LibConfig) {
     this.auth = this.getFirebaseAuth();
     onAuthStateChanged(this.getFirebaseAuth(), (user: User) => {
-      this.setLoggedInToLocalStorage(!!user)
       this.authState.next(user)
+    });
+
+    this.authState.subscribe(user => {
+      this.setLoggedInToLocalStorage(user);
     });
   }
 
@@ -58,19 +64,31 @@ export class AuthFirebaseService {
   public async logout(): Promise<boolean> {
     const auth = this.getFirebaseAuth();
     await auth.signOut();
+    this.authState.next(null);
     return true;
   }
 
-  public isLoggedInFromLocalStorage(): boolean {
-    if (typeof window !== 'undefined') {
-      const isLoggedIn = window.localStorage.getItem('isLoggedIn');
-      return isLoggedIn === 'true' ? true : false;
+  public isLoggedInFromLocalStorage(): User {
+    if (isPlatformBrowser(this.platformId)) {
+        const currentUser = window.localStorage.getItem('currentUser');
+
+        return currentUser ? JSON.parse(currentUser) as User : null;
+    } else {
+      return null;
     }
   }
 
-  public setLoggedInToLocalStorage(isLoggedIn: boolean): void {
-    if (typeof window !== 'undefined') {
-      window.localStorage.setItem('isLoggedIn', '' + isLoggedIn);
+  public setLoggedInToLocalStorage(user: User): void {
+    if (isPlatformBrowser(this.platformId)) {
+      window.localStorage.setItem('currentUser', '' + user ? JSON.stringify(user) : '');
+      if (user) {
+        user.getIdToken().then(token => {
+          // TODO: if cookie already exists
+          if (token) {
+            window.document.cookie = 'Authorization=Bearer ' + token;
+          }
+        })
+      }
     }
   }
 }
