f6589d2c56168240af9c8f6a29fe7fd0d331ff15
Sunil Kumar <sunil.divyam@gmail.com>
2022-12-20 23:05:53 +0530

    Feature - Added 8 Guards for various roles
    RoleAuthorGuard,
        RoleAdminGuard,
        RoleReaderGuard,
        RoleEditorGuard,
        RoleManagerGuard,
        RolePaidMemberGuard,
        RoleStudentGuard,
        RoleTeacherGuard,

--- projects/annu-ng-lib/src/lib/services/route-guards/route-guards.module.ts
+++ projects/annu-ng-lib/src/lib/services/route-guards/route-guards.module.ts
@@ -1,6 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IsLoggedInGuard } from './is-logged-in.guard';
+import { RoleAuthorGuard } from './role-author.guard';
+import { RoleAdminGuard } from './role-admin.guard';
+import { RoleReaderGuard } from './role-reader.guard';
+import { RoleEditorGuard } from './role-editor.guard';
+import { RoleManagerGuard } from './role-manager.guard';
+import { RolePaidMemberGuard } from './role-paid-member.guard';
+import { RoleStudentGuard } from './role-student.guard';
+import { RoleTeacherGuard } from './role-teacher.guard';
 
 
 
@@ -9,6 +17,16 @@ import { IsLoggedInGuard } from './is-logged-in.guard';
   imports: [
     CommonModule
   ],
-  providers: [IsLoggedInGuard],
+  providers: [
+    IsLoggedInGuard,
+    RoleAuthorGuard,
+    RoleAdminGuard,
+    RoleReaderGuard,
+    RoleEditorGuard,
+    RoleManagerGuard,
+    RolePaidMemberGuard,
+    RoleStudentGuard,
+    RoleTeacherGuard,
+  ],
 })
 export class RouteGuardsModule { }
