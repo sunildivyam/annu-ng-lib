b470bbcf785cc99d52d623ab4a250b065b662073
Sunil Kumar <sunil.divyam@gmail.com>
2022-12-22 23:42:43 +0530

    Enhancement - ColorPalette Component - added dynamic light for colors

--- projects/annu-ng-lib/src/lib/components/common-ui/color-palette/color-palette.component.ts
+++ projects/annu-ng-lib/src/lib/components/common-ui/color-palette/color-palette.component.ts
@@ -1,6 +1,6 @@
 import { Component, Output, EventEmitter, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { ThemeService } from '../theme/theme.service';
-import { FormControl } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, FormBuilder } from '@angular/forms';
 import { ColorPalette } from './color-palette.interface';
 
 /**
@@ -36,43 +36,86 @@ export class ColorPaletteComponent implements OnInit, OnChanges {
    * A property bound to slider form control, that allow to get/set saturation value
    */
   saturationControl: FormControl;
+
+  /**
+   * Property bound to slider form controls group, that allow to get/set light value to all colors
+   */
+  lightsForm: FormGroup;
+
   /**
    * A property bound to header title of component palette name postfixed with Pallete. eg. "Primary Palette"
    */
   headerText: string;
 
-  constructor(private themeService: ThemeService) {
+  constructor(private themeService: ThemeService, private fb: FormBuilder) {
+    this.colorPalette.colors = this.themeService.getPaletteColors(this.colorPalette.hue, this.colorPalette.saturation);
+    this.lightsForm = this.fb.group({
+      name: 'lightsGroup',
+      lightControls: this.fb.array([])
+    });
+  }
+
+
+  public get lightControls() : FormArray {
+    return this.lightsForm.get('lightControls') as FormArray;
+  }
+
+  private initFormControls() {
+    //Initialize form controls
     this.hueControl = new FormControl(this.colorPalette.hue);
     this.saturationControl = new FormControl(this.colorPalette.saturation);
+    this.colorPalette.colors.forEach(cl => this.lightControls.push(new FormControl(cl.l)));
+
+    // Subscribe to valueChange of form controls
     this.hueControl.valueChanges.subscribe(this.hueChanged);
     this.saturationControl.valueChanges.subscribe(this.saturationChanged);
+    this.lightControls.valueChanges.subscribe(this.lightChanged);
+
+    // this.lightControls.forEach(ctrl => ctrl.valueChanges.subscribe(this.lightChanged));
   }
+
   /**
    * Builds the palette from current hue and saturation, and emits the valueChanges event, with the colorPalette object.
    */
-  public  buildPalette(): void {
+  public  updateColorPalette(hue: number | undefined | null, saturation: number | undefined | null, lights: Array<number> | undefined | null = undefined): void {
     this.headerText = `${this.colorPalette.name ? this.colorPalette.name : 'Default'} Palette`;
-    this.colorPalette.colors = this.themeService.getPaletteColors(this.colorPalette.hue, this.colorPalette.saturation);
+    this.colorPalette.colors.forEach((cl, index) => {
+      cl.h = hue === cl.h ? cl.h : hue;
+      cl.s = saturation === cl.s ? cl.s : saturation;
+      if (lights instanceof Array) {
+        cl.l = lights[index];
+      }
+      cl.hsl = `hsl(${cl.h}, ${cl.s}%, ${cl.l}%)`
+    });
+
+    this.colorPalette.hue = hue;
+    this.colorPalette.saturation = saturation;
     this.valueChanges.emit(this.colorPalette);
   }
 
   public hueChanged = (hue: number) => {
     this.colorPalette.hue = hue;
-    this.buildPalette()
+    this.updateColorPalette(hue, this.colorPalette.saturation);
   }
 
   public saturationChanged = (saturation: number) => {
     this.colorPalette.saturation = saturation;
-    this.buildPalette();
+    this.updateColorPalette(this.colorPalette.hue, saturation);
+  }
+
+  public lightChanged = (lights: Array<number>) => {
+    this.updateColorPalette(this.colorPalette.hue, this.colorPalette.saturation, lights);
   }
 
   public ngOnInit(): void {
-    this.buildPalette();
+    this.initFormControls();
+    this.updateColorPalette(this.colorPalette.hue, this.colorPalette.saturation);
   }
 
-  public ngOnChanges(): void {
-    this.hueControl.setValue(this.colorPalette.hue);
-    this.saturationControl.setValue(this.colorPalette.saturation);
-    this.buildPalette();
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (this.colorPalette && !this.colorPalette?.colors?.length) {
+      this.colorPalette.colors = this.themeService.getPaletteColors(this.colorPalette.hue, this.colorPalette.saturation);
+      this.lightControls.controls.forEach((ctrl, index) => ctrl.setValue(this.colorPalette.colors[index].l, {emitEvent: false}));
+    }
   }
 }
