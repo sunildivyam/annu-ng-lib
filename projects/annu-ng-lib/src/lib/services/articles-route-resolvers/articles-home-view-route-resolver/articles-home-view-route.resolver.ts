a272ceaa434557ffdc36fc72df2cd6224786d726
Sunil Kumar <sunil.divyam@gmail.com>
2022-12-27 14:54:36 +0530

    Removed console.logs from Route Resolvers

--- projects/annu-ng-lib/src/lib/services/articles-route-resolvers/articles-home-view-route-resolver/articles-home-view-route.resolver.ts
+++ projects/annu-ng-lib/src/lib/services/articles-route-resolvers/articles-home-view-route-resolver/articles-home-view-route.resolver.ts
@@ -33,39 +33,28 @@ export class ArticlesHomeViewRouteResolver implements Resolve<ArticlesHomeViewRo
   constructor(private articlesFireSvc: ArticlesFirebaseService, private transferState: TransferState, @Inject(PLATFORM_ID) private platformId) { }
 
   async resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<ArticlesHomeViewRouteData> {
-    console.log(this.LOGS_MODULE_NAME, ' *** Is SERVER: ', isPlatformServer(this.platformId))
     this.routeData = {};
     // create a unique key that holds the route stata data.
     const HOME_VIEW_ROUTE_KEY = makeStateKey<ArticlesHomeViewRouteData>('articles-home-view-route');
 
     //Check if state data already exists, if yes, serve it from state, and clear the state else, fetch the data and set it to state, that can be used at client side.
     if (this.transferState.hasKey(HOME_VIEW_ROUTE_KEY)) {
-      console.log(this.LOGS_MODULE_NAME, ' *** - READING TRASFER STATE - START')
       this.routeData = this.transferState.get<ArticlesHomeViewRouteData>(HOME_VIEW_ROUTE_KEY, {});
-      console.log(this.LOGS_MODULE_NAME, ' *** - READING TRASFER STATE - END', ' *** CAT GROUPS COUNT:',  this.routeData.allCategoriesGroups.length)
       this.transferState.remove(HOME_VIEW_ROUTE_KEY);
-      console.log(this.LOGS_MODULE_NAME, ' *** - TRASFER STATE - REMOVED')
       return this.routeData;
     } else {
-      console.log(this.LOGS_MODULE_NAME, ' *** - READING FIRESTORE - START')
       await this.loadRouteData();
-      console.log(this.LOGS_MODULE_NAME, ' *** - READING FIRESTORE - END')
       if (isPlatformServer(this.platformId)) {
-        console.log(this.LOGS_MODULE_NAME, ' *** - SAVING TRANSFER STATE - START')
         this.transferState.set(HOME_VIEW_ROUTE_KEY, this.routeData);
-        console.log(this.LOGS_MODULE_NAME, ' *** - SAVING TRANSFER STATE - END')
       }
-      console.log(this.LOGS_MODULE_NAME, ' *** - READING TRASFER STATE - END', ' *** CAT GROUPS COUNT:',  this.routeData.allCategoriesGroups.length)
       return this.routeData;
     }
   }
 
   private async loadRouteData() {
     try {
-      console.log(this.LOGS_MODULE_NAME, ' *** - READING ALL CATEGORIES - START')
       const allCategories = await this.articlesFireSvc.getCategories({ isLive: true, orderField: 'updated' })
       this.routeData.allCategoriesGroups = [];
-      console.log(this.LOGS_MODULE_NAME, ' *** - READING ALL CATEGORIES - END')
       await Promise.all(allCategories.map(async (cat: Category) => {
         const queryConfig: QueryConfig = {
           isLive: true,
@@ -76,7 +65,6 @@ export class ArticlesHomeViewRouteResolver implements Resolve<ArticlesHomeViewRo
           startPage: null
         };
         const articles = await this.articlesFireSvc.getArticles(queryConfig);
-        console.log(this.LOGS_MODULE_NAME, ' *** - READ CATEGORY ARTICLES - CAT ID:', cat.id)
         this.routeData.allCategoriesGroups.push(
           {
             category: cat,
--- projects/annu-ng-lib/src/lib/services/lib-docs-route-resolvers/lib-docs-home-view-route-resolver/lib-docs-home-view-route.resolver.ts
+++ projects/annu-ng-lib/src/lib/services/lib-docs-route-resolvers/lib-docs-home-view-route-resolver/lib-docs-home-view-route.resolver.ts
@@ -30,38 +30,27 @@ export class LibDocsHomeViewRouteResolver implements Resolve<LibDocsHomeViewRout
   constructor(private docsService: DocsService, private transferState: TransferState, @Inject(PLATFORM_ID) private platformId) { }
 
   async resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<LibDocsHomeViewRouteData> {
-    console.log(this.LOGS_MODULE_NAME, ' *** Is SERVER: ', isPlatformServer(this.platformId))
     this.routeData = {};
     // create a unique key that holds the route stata data.
     const HOME_VIEW_ROUTE_KEY = makeStateKey<LibDocsHomeViewRouteData>('lib-docs-home-view-route');
 
     //Check if state data already exists, if yes, serve it from state, and clear the state else, fetch the data and set it to state, that can be used at client side.
     if (this.transferState.hasKey(HOME_VIEW_ROUTE_KEY)) {
-      console.log(this.LOGS_MODULE_NAME, ' *** - READING TRASFER STATE - START')
       this.routeData = this.transferState.get<LibDocsHomeViewRouteData>(HOME_VIEW_ROUTE_KEY, {});
-      console.log(this.LOGS_MODULE_NAME, ' *** - READING TRASFER STATE - END')
       this.transferState.remove(HOME_VIEW_ROUTE_KEY);
-      console.log(this.LOGS_MODULE_NAME, ' *** - TRASFER STATE - REMOVED')
       return this.routeData;
     } else {
-      console.log(this.LOGS_MODULE_NAME, ' *** - READING DOCs JSON - START')
       await this.loadRouteData();
-      console.log(this.LOGS_MODULE_NAME, ' *** - READING DOCs JSON - END')
       if (isPlatformServer(this.platformId)) {
-        console.log(this.LOGS_MODULE_NAME, ' *** - SAVING TRANSFER STATE - START')
         this.transferState.set(HOME_VIEW_ROUTE_KEY, this.routeData);
-        console.log(this.LOGS_MODULE_NAME, ' *** - SAVING TRANSFER STATE - END')
       }
-      console.log(this.LOGS_MODULE_NAME, ' *** - READING TRASFER STATE - END')
       return this.routeData;
     }
   }
 
   private async loadRouteData() {
     try {
-      console.log(this.LOGS_MODULE_NAME, ' *** - READING ALL CATEGORIES - START')
       this.routeData.libDocsInfo = await this.docsService.getLibDocsInfo();
-      console.log(this.LOGS_MODULE_NAME, ' *** - READING ALL CATEGORIES - END')
     } catch (error: any) {
       this.routeData.libDocsInfo = null;
       this.routeData.libDocsInfoError = error;
