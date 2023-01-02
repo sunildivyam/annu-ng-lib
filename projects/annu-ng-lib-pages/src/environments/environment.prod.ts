e827f810350ecad08e77d28b65187ef29f073da4
Sunil Kumar <sunil.divyam@gmail.com>
2023-01-02 20:03:34 +0530

    Update cat resolver

--- projects/annu-ng-lib-pages/src/environments/environment.prod.ts
+++ projects/annu-ng-lib-pages/src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
     apiBaseUrl: 'http://localhost:5000',  // This should be the hosted url
     docsJsonUrl: '/data/documentation.json',
     firebaseui: { ...firebaseui },
-    firebase: { ...firebaseAppConfig, apiKey: '' },
+    firebase: { ...firebaseAppConfig, apiKey: 'AIzaSyDlzVES09CQFxt8oJI0u2srZFpH12wOAKk' },
     firebaseStoreConfig,
   } as LibConfig
 };
