9dce502a1e6c0a3f4ce03d8978ec4120cd8e8ca5
Sunil Kumar <sunil.divyam@gmail.com>
2023-06-09 16:56:39 +0530

    LIB: Version 1.0.17 release

--- projects/annu-ng-lib/src/lib/components/cms/category/category.interface.ts
+++ projects/annu-ng-lib/src/lib/components/cms/category/category.interface.ts
@@ -1,29 +1,32 @@
-import { ImageInfo } from "../../common-ui/image-form";
-import { MetaInfo } from "../../common-ui/meta";
+import { ImageInfo } from '../../common-ui/image-form';
+import { MetaInfo } from '../../common-ui/meta';
 
 export interface Category {
-    id?: string;
-    shortTitle?: string;
-    isFeatured?: boolean;   // true, if featured category, this can be shown at primary locations on page.
-    metaInfo?: MetaInfo; // This info will be used for SEO for the page., title, description, keywords etc.
-    image?: ImageInfo,   // Thumbnail Image src
-    created?: string;
-    updated?: string;
-    userId?: string;    // User Id from Users
-    isLive?: boolean;
-    inReview?: boolean; // if category is up for review, if this is true, then isLive must be false;
-    features?: Array<CategoryFeatures | string>;   // any of the values from CategoryFeature, and any custom values.
-};
+  id?: string;
+  shortTitle?: string;
+  isFeatured?: boolean; // true, if featured category, this can be shown at primary locations on page.
+  metaInfo?: MetaInfo; // This info will be used for SEO for the page., title, description, keywords etc.
+  image?: ImageInfo; // Thumbnail Image src
+  created?: string;
+  updated?: string;
+  userId?: string; // User Id from Users
+  isLive?: boolean;
+  inReview?: boolean; // if category is up for review, if this is true, then isLive must be false;
+  features?: Array<CategoryFeatures | string>; // any of the values from CategoryFeature, and any custom values.
+}
 
 export enum CategoryFeatures {
-    primaryNavigation = 'primary-navigation',
-    footerNavigation = 'footer-navigation',
-    primeShow = 'prime-show',
-    primeShowAside = 'prime-show-aside',
-    inReview = 'in-review',
-    tnc = 'tnc',
-    privacy = 'privacy',
-    helpDocs = 'help-docs',
-    helpDocsAuthor = 'help-docs-author',
-    helpDocsAdmin = 'help-docs-admin',
+  primaryNavigation = 'primary-navigation',
+  footerNavigation = 'footer-navigation',
+  primeShow = 'prime-show',
+  primeShowAside = 'prime-show-aside',
+  inReview = 'in-review',
+  tnc = 'tnc',
+  privacy = 'privacy',
+  helpDocs = 'help-docs',
+  helpDocsAuthor = 'help-docs-author',
+  helpDocsAdmin = 'help-docs-admin',
+  aboutUs = 'about-us',
+  contactUs = 'contact-us',
+  vision = 'vision',
 }
